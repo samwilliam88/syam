32796dba400713d763b4d561f3a1f4ee8915d0e3
sam william <shyamappliedsnr@gmail.com>
2019-05-10 15:11:37 +0530

    second

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and redux
+          Learn React and redux....
         </a>
       </header>
     </div>
